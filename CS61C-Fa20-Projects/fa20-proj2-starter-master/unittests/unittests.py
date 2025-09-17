88cef164974b8a3304ff65a28bd4d1cc5e10d695
LiftingVermosh <fallingstar233@163.com>
2025-09-17 11:22:45 +0800

    feat(Project - 2:Part A)

--- CS61C-Fa20-Projects/fa20-proj2-starter-master/unittests/unittests.py
+++ CS61C-Fa20-Projects/fa20-proj2-starter-master/unittests/unittests.py
@@ -22,6 +22,13 @@ class TestAbs(TestCase):
         t.check_scalar("a0", 1)
         t.execute()
 
+    def test_minus_one(self):
+        t = AssemblyTest(self, "abs.s")
+        t.input_scalar("a0", -1)
+        t.call("abs")
+        t.check_scalar("a0", 1)
+        t.execute()
+
     @classmethod
     def tearDownClass(cls):
         print_coverage("abs.s", verbose=False)
@@ -50,20 +57,98 @@ class TestRelu(TestCase):
 
 class TestArgmax(TestCase):
     def test_simple(self):
+        # Test a simple array with unique maximum at the end
         t = AssemblyTest(self, "argmax.s")
-        # create an array in the data section
-        raise NotImplementedError("TODO")
-        # TODO
-        # load address of the array into register a0
-        # TODO
-        # set a1 to the length of the array
-        # TODO
-        # call the `argmax` function
-        # TODO
-        # check that the register a0 contains the correct output
-        # TODO
-        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        # Create an array in the data section
+        array = t.array([1, -2, 3, -4, 5, -6, 7, -8, 9])
+        # Load address of the array into register a0
+        t.input_array("a0", array)
+        # Set a1 to the length of the array
+        t.input_scalar("a1", len(array))
+        # Call the argmax function
+        t.call("argmax")
+        # Check that the register a0 contains the correct output (index 8)
+        t.check_scalar("a0", 8)
+        # Generate the assembly file and run it through venus
         t.execute()
+
+    def test_multiple_max(self):
+        # Test an array with multiple maximum elements, should return the smallest index
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([3, 3, 2, 3])  # Max value 3 at indices 0, 1, and 3; should return 0
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 0)  # Smallest index for max
+        t.execute()
+
+    def test_single_element(self):
+        # Test an array with only one element
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([5])
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 0)  # Only index 0
+        t.execute()
+
+    def test_all_same(self):
+        # Test an array where all elements are the same
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([4, 4, 4, 4])
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 0)  # Should return first index
+        t.execute()
+
+    def test_negative_numbers(self):
+        # Test an array with negative numbers and positive numbers
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([-1, -2, 0, 1])  # Max value 1 at index 3
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 3)
+        t.execute()
+
+    def test_max_at_start(self):
+        # Test where the maximum element is at the start of the array
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([10, 1, 2, 3])  # Max value 10 at index 0
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 0)
+        t.execute()
+
+    def test_max_at_end(self):
+        # Test where the maximum element is at the end of the array
+        t = AssemblyTest(self, "argmax.s")
+        array = t.array([1, 2, 3, 10])  # Max value 10 at index 3
+        t.input_array("a0", array)
+        t.input_scalar("a1", len(array))
+        t.call("argmax")
+        t.check_scalar("a0", 3)
+        t.execute()
+
+    def test_error_empty_array(self):
+        # Test error case: array length is 0, should terminate with code 77
+        t = AssemblyTest(self, "argmax.s")
+        # For empty array, we don't need to create an array, just set a1 to 0
+        t.input_scalar("a1", 0)
+        # Call argmax, but since length is 0, it should error
+        t.call("argmax")
+        # Execute and expect error code 77
+        t.execute(code=77)
+
+    def test_error_negative_length(self):
+        # Test error case: array length is negative, but since a1 is unsigned, this might not occur in practice, but we test for length < 1
+        # In RISC-V, a1 is passed as int, so negative length might be possible, but the function checks for < 1
+        t = AssemblyTest(self, "argmax.s")
+        t.input_scalar("a1", -1)  # Negative length
+        t.call("argmax")
+        t.execute(code=77)  # Should terminate with error code 77
 
     @classmethod
     def tearDownClass(cls):
@@ -72,27 +157,157 @@ class TestArgmax(TestCase):
 
 class TestDot(TestCase):
     def test_simple(self):
+        # Test simple dot product with stride 1 for both vectors
         t = AssemblyTest(self, "dot.s")
-        # create arrays in the data section
-        raise NotImplementedError("TODO")
-        # TODO
-        # load array addresses into argument registers
-        # TODO
-        # load array attributes into argument registers
-        # TODO
-        # call the `dot` function
+        # Create arrays in the data section
+        v0 = t.array([1, 2, 3, 4])
+        v1 = t.array([2, 3, 4, 5])
+        # Load array addresses into argument registers
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        # Load array attributes into argument registers
+        t.input_scalar("a2", 4)  # length
+        t.input_scalar("a3", 1)  # stride for v0
+        t.input_scalar("a4", 1)  # stride for v1
+        # Call the dot function
         t.call("dot")
-        # check the return value
-        # TODO
+        # Check the return value: 1*2 + 2*3 + 3*4 + 4*5 = 2+6+12+20=40
+        t.check_scalar("a0", 40)
         t.execute()
+
+    def test_stride_not_one(self):
+        # Test dot product with different strides
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2, 3, 4, 5, 6])
+        v1 = t.array([2, 3, 4, 5, 6, 7])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 3)  # length: we'll take every other element
+        t.input_scalar("a3", 2)  # stride for v0: skip one element
+        t.input_scalar("a4", 2)  # stride for v1: skip one element
+        t.call("dot")
+        # Elements: v0[0]=1, v0[2]=3, v0[4]=5; v1[0]=2, v1[2]=4, v1[4]=6
+        # Dot product: 1*2 + 3*4 + 5*6 = 2 + 12 + 30 = 44
+        t.check_scalar("a0", 44)
+        t.execute()
+
+    def test_different_strides(self):
+        # Test with different strides for v0 and v1
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2, 3, 4, 5, 6])
+        v1 = t.array([2, 3, 4, 5, 6, 7])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 2)  # length
+        t.input_scalar("a3", 1)  # stride for v0: consecutive
+        t.input_scalar("a4", 3)  # stride for v1: skip two elements
+        t.call("dot")
+        # v0: indices 0,1 -> [1,2]; v1: indices 0,3 -> [2,5]
+        # Dot product: 1*2 + 2*5 = 2 + 10 = 12
+        t.check_scalar("a0", 12)
+        t.execute()
+
+    def test_single_element(self):
+        # Test with single element vectors
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([5])
+        v1 = t.array([3])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 1)  # length
+        t.input_scalar("a3", 1)  # stride
+        t.input_scalar("a4", 1)  # stride
+        t.call("dot")
+        t.check_scalar("a0", 15)  # 5*3=15
+        t.execute()
+
+    def test_negative_numbers(self):
+        # Test with negative numbers
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([-1, 2, -3])
+        v1 = t.array([4, -5, 6])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 3)  # length
+        t.input_scalar("a3", 1)  # stride
+        t.input_scalar("a4", 1)  # stride
+        t.call("dot")
+        # (-1)*4 + 2*(-5) + (-3)*6 = -4 -10 -18 = -32
+        t.check_scalar("a0", -32)
+        t.execute()
+
+    def test_error_length_less_than_one(self):
+        # Test error case: length less than 1
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2])
+        v1 = t.array([3, 4])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 0)  # invalid length
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 1)
+        t.call("dot")
+        t.execute(code=75)  # should terminate with error code 75
+
+    def test_error_negative_length(self):
+        # Test error case: negative length (though a2 is signed, function checks <1)
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2])
+        v1 = t.array([3, 4])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", -1)  # invalid length
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 1)
+        t.call("dot")
+        t.execute(code=75)  # should terminate with error code 75
+
+    def test_error_stride_v0_less_than_one(self):
+        # Test error case: stride for v0 less than 1
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2])
+        v1 = t.array([3, 4])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 2)
+        t.input_scalar("a3", 0)  # invalid stride
+        t.input_scalar("a4", 1)
+        t.call("dot")
+        t.execute(code=76)  # should terminate with error code 76
+
+    def test_error_stride_v1_less_than_one(self):
+        # Test error case: stride for v1 less than 1
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2])
+        v1 = t.array([3, 4])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 2)
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 0)  # invalid stride
+        t.call("dot")
+        t.execute(code=76)  # should terminate with error code 76
+
+    def test_error_both_strides_invalid(self):
+        # Test error case: both strides invalid (should catch first error in code: v0 stride)
+        t = AssemblyTest(self, "dot.s")
+        v0 = t.array([1, 2])
+        v1 = t.array([3, 4])
+        t.input_array("a0", v0)
+        t.input_array("a1", v1)
+        t.input_scalar("a2", 2)
+        t.input_scalar("a3", 0)  # invalid stride
+        t.input_scalar("a4", 0)  # invalid stride
+        t.call("dot")
+        t.execute(code=76)  # should terminate with error code 76 (first check is v0 stride)
 
     @classmethod
     def tearDownClass(cls):
         print_coverage("dot.s", verbose=False)
 
 
-class TestMatmul(TestCase):
 
+class TestMatmul(TestCase):
     def do_matmul(self, m0, m0_rows, m0_cols, m1, m1_rows, m1_cols, result, code=0):
         t = AssemblyTest(self, "matmul.s")
         # we need to include (aka import) the dot.s file since it is used by matmul.s
@@ -104,31 +319,194 @@ class TestMatmul(TestCase):
         array_out = t.array([0] * len(result))
 
         # load address of input matrices and set their dimensions
-        raise NotImplementedError("TODO")
-        # TODO
+        t.input_array("a0", array0)  # pointer to m0
+        t.input_scalar("a1", m0_rows)  # rows of m0
+        t.input_scalar("a2", m0_cols)  # columns of m0
+        t.input_array("a3", array1)  # pointer to m1
+        t.input_scalar("a4", m1_rows)  # rows of m1
+        t.input_scalar("a5", m1_cols)  # columns of m1
         # load address of output array
-        # TODO
+        t.input_array("a6", array_out)  # pointer to output matrix d
 
         # call the matmul function
         t.call("matmul")
 
-        # check the content of the output array
-        # TODO
+        # check the content of the output array only if no error expected
+        if code == 0:
+            t.check_array(array_out, result)
 
         # generate the assembly file and run it through venus, we expect the simulation to exit with code `code`
         t.execute(code=code)
 
     def test_simple(self):
+        # Test simple 3x3 matrix multiplication
         self.do_matmul(
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [30, 36, 42, 66, 81, 96, 102, 126, 150]
         )
 
+    def test_2x2(self):
+        # Test 2x2 matrix multiplication
+        self.do_matmul(
+            [1, 2, 3, 4], 2, 2,
+            [5, 6, 7, 8], 2, 2,
+            [19, 22, 43, 50]
+        )
+
+    def test_1x1(self):
+        # Test 1x1 matrix multiplication (scalar multiplication)
+        self.do_matmul(
+            [5], 1, 1,
+            [3], 1, 1,
+            [15]
+        )
+
+    def test_rectangular(self):
+        # Test rectangular matrix multiplication: 2x3 * 3x2 = 2x2
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6], 2, 3,
+            [7, 8, 9, 10, 11, 12], 3, 2,
+            [58, 64, 139, 154]
+        )
+
+    def test_with_zeros(self):
+        # Test matrix multiplication with zeros
+        self.do_matmul(
+            [1, 0, 0, 1], 2, 2,
+            [2, 3, 4, 5], 2, 2,
+            [2, 3, 4, 5]  # Identity-like matrix multiplication
+        )
+
+    def test_negative_numbers(self):
+        # Test matrix multiplication with negative numbers
+        self.do_matmul(
+            [1, -2, 3, -4], 2, 2,
+            [-5, 6, -7, 8], 2, 2,
+            [1* -5 + -2 * -7, 1*6 + -2*8, 3* -5 + -4 * -7, 3*6 + -4*8]  # Calculate expected result
+        )
+
+    def test_error_m0_invalid_rows(self):
+        # Test error case: m0 has 0 rows
+        self.do_matmul(
+            [1, 2, 3, 4], 0, 2,  # 0 rows
+            [1, 2, 3, 4], 2, 2,
+            [0, 0, 0, 0],  # dummy result
+            code=72  # expected error code
+        )
+
+    def test_error_m0_invalid_cols(self):
+        # Test error case: m0 has 0 columns
+        self.do_matmul(
+            [1, 2, 3, 4], 2, 0,  # 0 columns
+            [1, 2, 3, 4], 2, 2,
+            [0, 0, 0, 0],  # dummy result
+            code=72  # expected error code
+        )
+
+    def test_error_m1_invalid_rows(self):
+        # Test error case: m1 has 0 rows
+        self.do_matmul(
+            [1, 2, 3, 4], 2, 2,
+            [1, 2, 3, 4], 0, 2,  # 0 rows
+            [0, 0, 0, 0],  # dummy result
+            code=73  # expected error code
+        )
+
+    def test_error_m1_invalid_cols(self):
+        # Test error case: m1 has 0 columns
+        self.do_matmul(
+            [1, 2, 3, 4], 2, 2,
+            [1, 2, 3, 4], 2, 0,  # 0 columns
+            [0, 0, 0, 0],  # dummy result
+            code=73  # expected error code
+        )
+
+    def test_error_dimension_mismatch(self):
+        # Test error case: m0 columns != m1 rows
+        self.do_matmul(
+            [1, 2, 3, 4], 2, 2,
+            [1, 2, 3, 4, 5, 6], 3, 2,  # 3 rows, but m0 has 2 columns
+            [0, 0, 0, 0],  # dummy result
+            code=74  # expected error code
+        )
+
+    def test_error_output_null(self):
+        # Test error case: output matrix pointer is null
+        t = AssemblyTest(self, "matmul.s")
+        t.include("dot.s")
+        
+        # Create input arrays
+        array0 = t.array([1, 2, 3, 4])
+        array1 = t.array([5, 6, 7, 8])
+        
+        # Set up valid inputs but null output pointer
+        t.input_array("a0", array0)
+        t.input_scalar("a1", 2)  # rows of m0
+        t.input_scalar("a2", 2)  # columns of m0
+        t.input_array("a3", array1)
+        t.input_scalar("a4", 2)  # rows of m1
+        t.input_scalar("a5", 2)  # columns of m1
+        t.input_scalar("a6", 0)  # null output pointer
+        
+        t.call("matmul")
+        t.execute(code=75)  # should terminate with error code 75
+
+    def test_row_vector(self):
+        # Test 1xN matrix multiplication (row vector)
+        self.do_matmul(
+            [1, 2, 3], 1, 3,  # row vector
+            [4, 5, 6, 7, 8, 9], 3, 2,  # 3x2 matrix
+            [1*4 + 2*6 + 3*8, 1*5 + 2*7 + 3*9]  # 1x2 result
+        )
+
+    def test_column_vector(self):
+        # Test Nx1 matrix multiplication (column vector)
+        self.do_matmul(
+            [1, 2, 3], 3, 1,  # column vector
+            [4], 1, 1,  # 1x1 matrix
+            [1*4, 2*4, 3*4]  # 3x1 result
+        )
+
+    def test_large_matrix(self):
+        # Test larger matrix to ensure loop logic works correctly
+        # 4x3 * 3x2 = 4x2
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 4, 3,
+            [1, 2, 3, 4, 5, 6], 3, 2,
+            [1*1+2*3+3*5, 1*2+2*4+3*6, 
+             4*1+5*3+6*5, 4*2+5*4+6*6,
+             7*1+8*3+9*5, 7*2+8*4+9*6,
+             10*1+11*3+12*5, 10*2+11*4+12*6]
+        )
+
+    def test_all_ones(self):
+        # Test matrix of all ones
+        self.do_matmul(
+            [1, 1, 1, 1], 2, 2,
+            [1, 1, 1, 1], 2, 2,
+            [2, 2, 2, 2]  # 2*1 + 2*1 = 4, but wait: 1*1+1*1=2, 1*1+1*1=2, etc.
+        )
+
+    def test_identity_matrix(self):
+        # Test identity matrix multiplication
+        self.do_matmul(
+            [1, 0, 0, 1], 2, 2,  # identity matrix
+            [2, 3, 4, 5], 2, 2,
+            [2, 3, 4, 5]  # should return the same matrix
+        )
+
+    def test_zero_matrix(self):
+        # Test zero matrix multiplication
+        self.do_matmul(
+            [0, 0, 0, 0], 2, 2,  # zero matrix
+            [1, 2, 3, 4], 2, 2,
+            [0, 0, 0, 0]  # should return zero matrix
+    )
+
     @classmethod
     def tearDownClass(cls):
         print_coverage("matmul.s", verbose=False)
-
 
 class TestReadMatrix(TestCase):
 
